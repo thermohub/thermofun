aeeaa999ca9da7acf18febc7d11ef4d433f579e8
dmiron <dan.miron@pis.ch>
2016-05-08 14:44:55 +0200

    commited the changes in the test main file for empirical Cp integration

--- tests/unit01/src/main.cpp
+++ tests/unit01/src/main.cpp
@@ -4,39 +4,35 @@
 #include "Substance.h"
 #include "ThermoModelsSubstance.h"
 #include "tcorrpt_global.h"
+#include "Thermo.h"
 
 using namespace std;
 using namespace TCorrPT;
 
 int main()
 {
-    /// variables substance ///
-    vd sV0,              ///< Standard volume at Tst, Pst "V0" m3/mol; [1] ERROR
-       sG0,              ///< Standard Gibbs energy at Tst, Pst  "GO" J/mol; [1] ERROR
-       sH0,              ///< Standard enthalpy at Tst, Pst  "H0" J/mol; [1] ERROR
-       sS0,              ///< Standard entropy of formation from elements at st.states "S0f" J/K/mol; [1] ERROR
-       saS0,             ///< Standard absolute entropy at Tst, Pst "S0" J/K/mol;[1] ERROR
-       sCp0;             ///< Standard constant-pressure heat capacity at Tst, Pst "Cp0" J/K/mol; [1] ERROR
+    /// thermo properties substance ///
+    double sV0,              ///< Standard volume at Tst, Pst "V0" m3/mol; [1] ERROR
+           sG0,              ///< Standard Gibbs energy at Tst, Pst  "GO" J/mol; [1] ERROR
+           sH0,              ///< Standard enthalpy at Tst, Pst  "H0" J/mol; [1] ERROR
+           sS0,              ///< Standard entropy of formation from elements at st.states "S0f" J/K/mol; [1] ERROR
+           saS0,             ///< Standard absolute entropy at Tst, Pst "S0" J/K/mol;[1] ERROR
+           sCp0;             ///< Standard constant-pressure heat capacity at Tst, Pst "Cp0" J/K/mol; [1] ERROR
 
-    vd G0, H0, S0,         ///< partial molal gibbs energy, enthalpy, entropy at current TP; [1] ERROR
-       V0, Cp0,               ///< partial molal volume, heat capacity at current TP; [1] ERROR
-       Fug, dVg;              ///< molar fugacity at TP (reserved), dVm of gas (reserved); [1] ERROR
+    double G0, H0, S0,         ///< partial molal gibbs energy, enthalpy, entropy at current TP; [1] ERROR
+           V0, Cp0,               ///< partial molal volume, heat capacity at current TP; [1] ERROR
+           Fug, dVg;              ///< molar fugacity at TP (reserved), dVm of gas (reserved); [1] ERROR
 
     double Tr = 298.15,                  ///< Reference temperature for standard state (usually 298.15 K)
            Pr = 1e05;                  ///< Reference pressure (usually 1 bar or 10^5 Pa, sometimes 1.013 bar = 1 atm)
     double currentP = 2000,                    ///<
            currentT = 400;                    ///<
 
-   /// END variables substance ///
+   /// END thermo properties ///
 
     cout << "Hello World!" << endl;
 
-    Substance subst;
-
-    ThermoModelsSubstance modsubst(subst);
-
-
-//    TCorrPT Tcorr = TCorrPT(1,2);
+    Database dtb;
 
 // +++ Test EmpCpIntegr +++
 //#define TEST_EMPCPINTEGR
@@ -48,45 +44,57 @@ int main()
 
     // Corundum
     // Reference properties
-    V0 = {2.558, 0};
-    G0 = {-1581808, 0};
-    H0 = {-1675250, 0};
-    S0 = {50.9, 0};
-    Cp0 = {79.4529, 0};
+    V0 = 2.558;
+    G0 = -1581808;
+    H0 = -1675250;
+    S0 = 50.9;
+    Cp0 = 79.4529;
 
     Interval = {0,2000};
     Coeff = {139.5, 0.00589, -2.4606e6, -589.2};
     TCpInterval.push_back(Interval);
     CpCoefficients.push_back(Coeff);
 
-    TSubstancePT Corundum;
+    Substance corundum;
+    corundum.setName("Corundum");
+    corundum.setFormula("Al2O3");
+    corundum.setSubstanceClass(SubstanceClass::type::COMPONENT);
+    corundum.setAggregateState(AggregateState::type::CRYSTAL);
 
-    SubstanceDataPrTr Corr_data_atPrTr;
-    SubstanceDataPT Corr_data_atPT;
+    ThermoPropertiesSubstance corr_data_atPrTr;
+    ThermoPropertiesSubstance result1, result2;
 
-    Corr_data_atPrTr.setReferencePb(1);
-    Corr_data_atPrTr.setReferenceTc(25);
-    Corr_data_atPrTr.setSV0(V0);
-    Corr_data_atPrTr.setSG0(G0);
-    Corr_data_atPrTr.setSH0(H0);
-    Corr_data_atPrTr.setSS0(S0);
-    Corr_data_atPrTr.setSCp0(Cp0);
+    corr_data_atPrTr.volume = V0;
+    corr_data_atPrTr.gibbs_energy =G0;
+    corr_data_atPrTr.enthalpy = H0;
+    corr_data_atPrTr.entropy = S0;
+    corr_data_atPrTr.heat_capacity_cp = Cp0;
 
-    Corr_data_atPT.setCurrentTc(currentT);
-    Corr_data_atPT.setCurrentPb(currentP);
+    corundum.setThermoReferenceProperties(corr_data_atPrTr);
+    corundum.setMethodGenEoS(MethodGenEoS_Thrift::type::CTPM_CPT);
 
-    Corundum.setDataAtPrTr(&Corr_data_atPrTr);
-    Corundum.setDataAtPT(&Corr_data_atPT);
-    Corundum.setMethod_genEoS(MethodGenEoS_Thrift::type::CTPM_CPT);
-    Corundum.setMethod_CorrT(MethodCorrT_Thrift::type::CTM_CST);
-    Corundum.setMethod_CorrP(MethodCorrP_Thrift::type::CPM_CEH);
-    Corundum.makeTCorrPT();
+    ThermoParametersSubstance param = corundum.thermoParameters();
 
-    TCorrModSubstance * TCorr = Corundum.getTCorr(0);
-    TCorr->setTCinterval(TCpInterval);
-    TCorr->setCpCoeff(CpCoefficients);
+    param.temperature_intervals = TCpInterval;
+    param.Cp_coeff = CpCoefficients;
 
-    TCorr->PTparam();
+    corundum.setThermoParameters(param);
+
+    ThermoModelsSubstance thermo_corundum (corundum);
+    result1 = thermo_corundum.thermoProperties(currentT, currentP);
+
+    // TCorrPT API
+
+    dtb.addSubstance(corundum);
+
+    Thermo thermo (dtb);
+
+    // Test error out of the interval
+//    currentT = 2005;
+    result2 = thermo.thermoProperties(currentT, currentP, "Corundum");
+
+    // Test error no substance
+//    result2 = thermo.thermoProperties(currentT, currentP, "alala");
 
 #endif
 // +++ END test CP +++
