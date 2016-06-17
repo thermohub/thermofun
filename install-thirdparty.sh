427813b937f872823aa47b6dff77b2272e12bf18
dmiron <dan.miron@psi.ch>
2016-06-17 18:10:26 +0200

    Added install third party files - which points to branch TCorrPT in bsonio repo.

---
+++ install-thirdparty.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+J=1
+
+while [[ $# > 2 ]]
+do
+key="$2"
+
+case $key in
+    -j)
+    J="$3"
+    shift
+    ;;
+    *)
+
+    ;;
+esac
+shift
+done
+
+CMAKE_FILE_DIR=$(pwd)/ThirdParty
+
+mkdir -p ../build/{debug,release}/thirdparty
+
+cd ../build/debug/thirdparty
+
+cmake $CMAKE_FILE_DIR -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH=$1
+
+make -j$J install
+
+cd ../../release/thirdparty
+
+cmake $CMAKE_FILE_DIR -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH=$1
+
+make -j$J install
