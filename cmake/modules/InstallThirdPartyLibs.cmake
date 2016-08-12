f7dca11a6472b32f7c70b56c395ff169c1d94587
dmiron <dan.miron@psi.ch>
2016-08-12 16:14:31 +0200

    added missing bsnoio to install third party

--- cmake/modules/InstallThirdPartyLibs.cmake
+++ cmake/modules/InstallThirdPartyLibs.cmake
@@ -11,6 +11,17 @@ if(NOT ${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     set(CXXFLAGS "-fPIC")
 endif()
 
+# Download and install the bsonio library
+ExternalProject_Add(BSONIO
+    PREFIX thirdparty
+    GIT_REPOSITORY https://dmiron@bitbucket.org/gems4/bsonio.git 
+    UPDATE_COMMAND ""
+    CMAKE_ARGS -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
+               -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_DIR}
+               -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+               -DBUILD_SHARED_LIBS=ON
+)
+
 
 # Create the install target for the third-party libraries
 install(DIRECTORY ${THIRDPARTY_DIR}/lib 
