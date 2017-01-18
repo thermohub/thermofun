e9fe19c862e68561a49ec70c0fef4d59546d0eed
dmiron <dan.miron@psi.ch>
2017-01-18 11:28:03 +0100

    Added test project for reactions

--- tests/interfaceTest/src/main.cpp
+++ tests/interfaceTest/src/main.cpp
@@ -7,17 +7,17 @@ int main(int argc, char *argv[])
 {
     cout << "Hello World!" << endl;
 
-    string databaseFile = "Resources/test_multi_new.VertexSubstance.json";
+    string databaseFile = "TestReactionsPSI.VertexReaction.json";
 
-    Interface thermo(databaseFile);
-    thermo.setSolventSymbolForAqSubst("H2O@");
+//    Interface thermo(databaseFile);
+//    thermo.setSolventSymbolForAqSubst("H2O@");
 
-    double T = 100;
-    double P = 50;
+//    double T = 100;
+//    double P = 50;
 
-    double S = thermo.calculateProperties("H2O@", 25, 1, "entropy").toDouble();
+//    double S = thermo.calculateProperties("H2O@", 25, 1, "entropy").toDouble();
 
-    thermo.calculateProperties({"Al+3", "OH-", "SiO2@"}, {"gibbs_energy","entropy", "volume"}, T, P).toCSV("results.csv");
+//    thermo.calculateProperties({"Al+3", "OH-", "SiO2@"}, {"gibbs_energy","entropy", "volume"}, T, P).toCSV("results.csv");
 
 //    API.addTP_pairs(25, 1);
 //    API.addSubstances(substanceSymbols);
---
+++ tests/reactionsTest/install-thirdparty.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+J=1
+
+while [[ $# > 2 ]]
+do
+key="$2"
+
+case $key in
+    -j)
+    J="$3"
+    shift
+    ;;
+    *)
+
+    ;;
+esac
+shift
+done
+
+CMAKE_FILE_DIR=$(pwd)/ThirdParty
+
+mkdir -p ../../../build-reactions-test/{debug,release}/thirdparty
+
+cd ../../../build-reactions-test/debug/thirdparty
+
+cmake $CMAKE_FILE_DIR -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH=$1
+
+make -j$J install
+
+cd ../../release/thirdparty
+
+cmake $CMAKE_FILE_DIR -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. -DCMAKE_PREFIX_PATH=$1
+
+make -j$J install
