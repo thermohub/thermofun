37c1a36d56194a5b194b7f0e43cf0bd6d531cccd
dmiron <dan.miron@psi.ch>
2017-04-07 17:05:43 +0200

    Added level to traversal

--- src/DBClient/Traversal.cpp
+++ src/DBClient/Traversal.cpp
@@ -4,6 +4,7 @@
 #include "Database.h"
 #include "Substance.h"
 #include "Reaction.h"
+#include "Common/Exception.h"
 
 using namespace bsonio;
 
@@ -15,29 +16,29 @@ Traversal::Traversal (boost::shared_ptr<bsonio::TDBGraph> _dbgraph)
     dbgraph = _dbgraph;
 }
 
-MapIdBson Traversal::getLinkedBsonFromSelectedData( vector<int> selNdx, vector<string> aKeyList)
+MapIdBson Traversal::getLinkedBsonFromSelectedData(vector<int> selNdx, vector<string> aKeyList, string level)
 {
     MapIdBson result;
     for( uint ii=0; ii<selNdx.size(); ii++ )
     {
-        linkedBsonDataFromId(aKeyList[selNdx[ii]], result);
+        linkedBsonDataFromId(aKeyList[selNdx[ii]], result, level);
     }
     return result;
 }
 
-MapIdBson Traversal::getLinkedBsonFromIdList( vector<string> idList)
+MapIdBson Traversal::getLinkedBsonFromIdList( vector<string> idList, string level)
 {
     MapIdBson result;
     for (uint ii=0; ii<idList.size(); ii++)
     {
-        linkedBsonDataFromId(idList[ii], result);
+        linkedBsonDataFromId(idList[ii], result, level);
     }
     return result;
 }
 
-void Traversal::linkedBsonDataFromId(std::string id_, MapIdBson &result)
+void Traversal::linkedBsonDataFromId(std::string id_, MapIdBson &result, string level)
 {
-    string valDB, _id, _label, _type;
+    string valDB, _id, _label, _type, _level, _symbol;
     bson record;
     id_ = id_.c_str();
     dbgraph->GetRecord( id_.c_str() );
@@ -47,6 +48,8 @@ void Traversal::linkedBsonDataFromId(std::string id_, MapIdBson &result)
     bsonio::bson_to_key( record.data, "_id",    _id);
     bsonio::bson_to_key( record.data, "_label", _label);
     bsonio::bson_to_key( record.data, "_type",  _type);
+    bsonio::bson_to_key( record.data, "properties.level",  _level);
+    bsonio::bson_to_key( record.data, "properties.symbol",  _symbol);
 
     // if not in the database
     if (!result.count(_id))
@@ -54,18 +57,23 @@ void Traversal::linkedBsonDataFromId(std::string id_, MapIdBson &result)
         if (_label == "substance")
         {
             result.insert(std::pair<std::string,std::string>(_id, "substance"));
-            followIncomingDefines(_id, result);
+            followIncomingDefines(_id, result, level);
         }
         if (_label == "reaction")
         {
+            // Check if levels match
+            if (level !=  _level)
+            {
+                errorNotMatchingLevel(_label, (_symbol + " level " + _level + " traversal level " + level), __LINE__, __FILE__);// error level of traversal
+            }
             result.insert(std::pair<std::string,std::string>(_id, "reaction"));
-            followIncomingTakes(_id, result);
+            followIncomingTakes(_id, result, level);
         }
     }
 
 }
 
-Database Traversal::getDatabaseFromTraversal(MapIdBson resultTraversal)
+Database Traversal::getDatabaseFromTraversal(MapIdBson resultTraversal, string level)
 {
     string key, valDB, _idSubst;
     bson record;
@@ -92,20 +100,18 @@ Database Traversal::getDatabaseFromTraversal(MapIdBson resultTraversal)
 
             Substance substance = parseSubstance(record.data);
 
-            if ( substances_map.find(substance.symbol()) == substances_map.end() ) {
-                substances_map[substance.symbol()] = substance;
-            } else {
-                // ERROR substance with the same symbol found!
-            }
-
             // get reaction symbol which define substance with _idSubst
-            if (getDefinesReactionSymbol(_idSubst) != "")
+            if (getDefinesReactionSymbol(_idSubst, level) != "")
             {
-                substance.setReactionSymbol(getDefinesReactionSymbol(_idSubst));
+                substance.setReactionSymbol(getDefinesReactionSymbol(_idSubst, level));
                 substance.setThermoCalculationType(ThermoFun::SubstanceThermoCalculationType::type::REACDC);
             }
 
-            substances_map[substance.symbol()] = substance;
+            if ( substances_map.find(substance.symbol()) == substances_map.end() ) {
+                substances_map[substance.symbol()] = substance;
+            } else {
+                errorSameSymbol("substance", substance.symbol(), __LINE__, __FILE__ );
+            }
         } else
             if (iterator->second == "reaction")
             {
@@ -118,16 +124,14 @@ Database Traversal::getDatabaseFromTraversal(MapIdBson resultTraversal)
 
                 Reaction reaction = ThermoFun::parseReaction(record.data);
 
+                // get reactants by following reaction incoming takes edge
+                reaction.setReactants(getReactantsCoeffMap(key, level));
+
                 if ( reactions_map.find(reaction.symbol()) == reactions_map.end() ) {
                     reactions_map[reaction.symbol()] = reaction;
                 } else {
-                    // ERROR reaction with the same symbol found!
+                    errorSameSymbol("reaction", reaction.symbol(), __LINE__, __FILE__ );
                 }
-
-                // get reactants by following reaction incoming takes edge
-                reaction.setReactants(getReactantsCoeffMap(key));
-
-                reactions_map[reaction.symbol()] = reaction;
             }
     }
     tdb.addMapReactions(reactions_map);
@@ -135,13 +139,18 @@ Database Traversal::getDatabaseFromTraversal(MapIdBson resultTraversal)
     return tdb;
 }
 
-void Traversal::followIncomingDefines(std::string _idSubst, MapIdBson &result)
+void Traversal::followIncomingDefines(std::string _idSubst, MapIdBson &result, string level)
 {
     string _idReac = ""; string kbuf = "";
     bson record;
+//    string qrJson = "{'_type': 'edge', '_label': 'defines', '_inV': '";
+//    qrJson += _idSubst;
+//    qrJson += "' }";
     string qrJson = "{'_type': 'edge', '_label': 'defines', '_inV': '";
-    qrJson += _idSubst;
-    qrJson += "' }";
+    qrJson += (_idSubst + "',");
+    qrJson += "'$and' : [ { 'properties.level' : ";
+    qrJson += level;
+    qrJson += "}]}";
 
     vector<string> _queryFields = { "_outV", "_label"};
     vector<string> _resultDataEdge, _resultDataReac;
@@ -163,19 +172,24 @@ void Traversal::followIncomingDefines(std::string _idSubst, MapIdBson &result)
             if (!result.count(_idReac))
             {
                 result.insert((std::pair<std::string,std::string>(_idReac, "reaction")));
-                followIncomingTakes(_idReac, result);
+                followIncomingTakes(_idReac, result, level);
             }
         }
     }
 }
 
-void Traversal::followIncomingTakes(std::string _idReac, MapIdBson &result)
+void Traversal::followIncomingTakes(std::string _idReac, MapIdBson &result, string level)
 {
     string _idSubst = ""; string kbuf = "";
     bson record;
+//    string qrJson = "{'_type': 'edge', '_label': 'takes', '_inV': '";
+//    qrJson += _idReac;
+//    qrJson += "' }";
     string qrJson = "{'_type': 'edge', '_label': 'takes', '_inV': '";
-    qrJson += _idReac;
-    qrJson += "' }";
+    qrJson += (_idReac + "',");
+    qrJson += "'$and' : [ { 'properties.level' : ";
+    qrJson += level;
+    qrJson += "}]}";
 
     vector<string> _queryFields = { "_outV", "_label"};
     vector<string> _resultDataEdge, _resultDataSubst;
@@ -197,20 +211,25 @@ void Traversal::followIncomingTakes(std::string _idReac, MapIdBson &result)
             if (!result.count(_idSubst))
             {
                 result.insert((std::pair<std::string,std::string>(_idSubst, "substance")));
-                followIncomingDefines(_idSubst, result);
+                followIncomingDefines(_idSubst, result, level);
             }
         }
     }
 
 }
 
-std::string Traversal::getDefinesReactionSymbol(std::string _idSubst)
+std::string Traversal::getDefinesReactionSymbol(std::string _idSubst, string level)
 {
     string kbuf = "";
     bson record;
+//    string qrJson = "{'_type': 'edge', '_label': 'defines', '_inV': '";
+//    qrJson += _idSubst;
+//    qrJson += "' }";
     string qrJson = "{'_type': 'edge', '_label': 'defines', '_inV': '";
-    qrJson += _idSubst;
-    qrJson += "' }";
+    qrJson += (_idSubst + "',");
+    qrJson += "'$and' : [ { 'properties.level' : ";
+    qrJson += level;
+    qrJson += "}]}";
 
     vector<string> _queryFields = { "_outV", "_label"};
     vector<string> _resultDataEdge, _resultDataReac;
@@ -232,15 +251,20 @@ std::string Traversal::getDefinesReactionSymbol(std::string _idSubst)
     return kbuf;
 }
 
-std::map<std::string, double> Traversal::getReactantsCoeffMap(std::string _id)
+std::map<std::string, double> Traversal::getReactantsCoeffMap(std::string _id, string level)
 {
     std::map<std::string, double> map;
     string kbuf;
     double stoi_coeff;
     bson record;
+//    string qrJson = "{'_type': 'edge', '_label': 'takes', '_inV': '";
+//    qrJson += _id;
+//    qrJson += "' }";
     string qrJson = "{'_type': 'edge', '_label': 'takes', '_inV': '";
-    qrJson += _id;
-    qrJson += "' }";
+    qrJson += (_id+ "',");
+    qrJson += "'$and' : [ { 'properties.level' : ";
+    qrJson += level;
+    qrJson += "}]}";
 
     vector<string> _queryFields = { "_outV", "properties.stoi_coeff"};
     vector<string> _resultDataEdge, _resultDataSubst;
--- src/DBClient/Traversal.h
+++ src/DBClient/Traversal.h
@@ -16,23 +16,23 @@ class Traversal
 {
     boost::shared_ptr<bsonio::TDBGraph> dbgraph;
 
-    void followIncomingDefines(std::string _idSubst, MapIdBson &result);
-    void followIncomingTakes(std::string _idReac, MapIdBson &result);
+    void followIncomingDefines(std::string _idSubst, MapIdBson &result, string level);
+    void followIncomingTakes(std::string _idReac, MapIdBson &result, string level);
 
-    std::string getDefinesReactionSymbol(std::string _idSubst);
-    std::map<std::string, double> getReactantsCoeffMap(std::string _id);
+    std::string getDefinesReactionSymbol(std::string _idSubst, string level);
+    std::map<std::string, double> getReactantsCoeffMap(std::string _id, string level);
 
-    void linkedBsonDataFromId(std::string id_, MapIdBson &result);
+    void linkedBsonDataFromId(std::string id_, MapIdBson &result, string level);
 
     public:
 
     explicit Traversal(boost::shared_ptr<bsonio::TDBGraph> _dbgraph);
 //    ~Traversal();
 
-    MapIdBson getLinkedBsonFromSelectedData(vector<int> selNdx, vector<string> aKeyList);
-    MapIdBson getLinkedBsonFromIdList( vector<string> idList);
+    MapIdBson getLinkedBsonFromSelectedData(vector<int> selNdx, vector<string> aKeyList, string level);
+    MapIdBson getLinkedBsonFromIdList(vector<string> idList, string level);
 
-    Database getDatabaseFromTraversal(MapIdBson resultTraversal);
+    Database getDatabaseFromTraversal(MapIdBson resultTraversal, string level);
 };
 
 }
