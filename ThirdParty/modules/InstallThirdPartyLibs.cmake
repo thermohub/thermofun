1abf6fd61387c2b5fc394ea41b5dccff127954f7
svetad <svd@ciklum.com>
2017-11-30 15:30:06 +0200

    fixed install

--- ThirdParty/modules/InstallThirdPartyLibs.cmake
+++ ThirdParty/modules/InstallThirdPartyLibs.cmake
@@ -11,7 +11,7 @@ ExternalProject_Add(BSONIO
     PREFIX thirdparty
     GIT_REPOSITORY https://dmiron@bitbucket.org/gems4/bsonio.git 
     GIT_TAG new-database
-    UPDATE_COMMAND ""
+#    UPDATE_COMMAND ""
     CMAKE_ARGS -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
                -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_DIR}
                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
--- cmake/modules/InstallThirdPartyLibs.cmake
+++ cmake/modules/InstallThirdPartyLibs.cmake
@@ -15,6 +15,7 @@ endif()
 ExternalProject_Add(BSONIO
     PREFIX thirdparty
     GIT_REPOSITORY https://dmiron@bitbucket.org/gems4/bsonio.git 
+    GIT_TAG new-database
     UPDATE_COMMAND ""
     CMAKE_ARGS -DCMAKE_PREFIX_PATH:PATH=${CMAKE_PREFIX_PATH}
                -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_DIR}
