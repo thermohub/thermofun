b23d9e1d1fa7a6b34384516f41b68d91f5d08fb3
dmiron <mirondanro@yahoo.com>
2018-11-26 09:31:59 +0100

    work on python interface

---
+++ python/pyThermoFun/Batch/pyOutputBatch.cpp
@@ -0,0 +1,43 @@
+// This file is part of ThermoFun https://bitbucket.org/gems4/thermofun/
+// ThermoFun is a framework for delivering standard state thermodynamic data.
+//
+// Copyright (c) 2016-2018 G.D.Miron, D.A.Kulik, A.Leal 
+//
+// ThermoFun is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+
+// ThermoFun is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with ThermoFun code. If not, see <http://www.gnu.org/licenses/>.
+
+// pybind11 includes
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+namespace py = pybind11;
+
+// ThermoFun includes
+#include <thermofun/Batch/ThermoBatch.h>
+#include <thermofun/Batch/OutputBatch.h>
+//#include <thermofun/Database.h>
+//#include <thermofun/ThermoProperties.h>
+
+namespace ThermoFun {
+
+void exportOutput(py::module& m)
+{
+    py::class_<Output>(m, "Output")
+        .def(py::init<const ThermoBatch&>())
+        .def("toCSV", &Output::toCSV)
+        .def("toCSVTransposed", &Output::toCSVTransposed)
+        .def("toDouble", &Output::toDouble)
+        .def("toThermoScalar", &Output::toThermoScalar)
+        .def("to2DVectorDouble", &Output::to2DVectorDouble)                      
+        ;
+}
+}
