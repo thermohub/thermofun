f02c900e6fe35b65275d265ab2f58d618579d09b
dmiron <mirondanro@yahoo.com>
2019-07-16 17:32:46 +0200

    added example for solute-hp98 model, fixes

--- ThermoFun/Substances/Solute/SoluteHollandPowell98.cpp
+++ ThermoFun/Substances/Solute/SoluteHollandPowell98.cpp
@@ -13,31 +13,41 @@ auto thermoPropertiesAqSoluteHP98(Reaktoro_::Temperature TK, Reaktoro_::Pressure
     double Tprime = 0.0;
     ThermoPropertiesSubstance tps;
     auto tpsr = subst.thermoReferenceProperties();
-    auto T298 = subst.referenceT();
-    auto G298 = tpsr.gibbs_energy;
-    auto H298 = tpsr.enthalpy;
-    auto S298 = tpsr.entropy;
-    auto V298 = tpsr.volume; // J/bar
-    auto Cp298 = tpsr.heat_capacity_cp;
+    double T298 = subst.referenceT();
+    double G298 = tpsr.gibbs_energy.val;
+    double H298 = tpsr.enthalpy.val;
+    double S298 = tpsr.entropy.val;
+    double V298 = tpsr.volume.val; // J/bar
+    double Cp298 = tpsr.heat_capacity_cp.val;
 
 //    // props given in HP98 page 314
 //    ALPw298 = 0.0002593; // 1/K
 //    BETw298 = 0.00004523; // 1/bar
 //    dALPdTw298 = 0.0000095714; // 1/K^2
 //    RHOw298 = 0.997; // g/cm^3
-    auto ALPw298 = wpr.Alpha;
-    auto BETw298 = wpr.Beta;
-    auto dALPdTw298 = wpr.dAldT;
-    auto RHOw298 = wpr.density;
+    double ALPw298 = wpr.Alpha.val;
+    double BETw298 = wpr.Beta.val*1e5; // 1/bar
+    double dALPdTw298 = wpr.dAldT.val;
+    double RHOw298 = wpr.density.val/1000; // g/cm3
 
     const auto HP98param = subst.thermoParameters().solute_holland_powell98_coeff;
 
     // EOS coefficients
-    auto b = HP98param[0];
+    if (HP98param.size()<1)
+    {
+        Exception exception;
+        exception.error << "Error in Holland and Powell aq solute model";
+        exception.reason << "There are no model parameters given for "<<subst.symbol() << ".";
+        exception.file = __FILE__;
+        exception.line = __LINE__;
+        RaiseError(exception);
+    }
 
-    auto RHOw = wp.density;
+
+    auto b = HP98param[0];
+    auto RHOw = wp.density/1000; // g/cm3
     auto ALPw = wp.Alpha;
-    auto BETw = wp.Beta;
+    auto BETw = wp.Beta*1e5; // 1/bar
     auto dALPdTw = wp.dAldT;
 
     if (T <= 500)
@@ -48,9 +58,8 @@ auto thermoPropertiesAqSoluteHP98(Reaktoro_::Temperature TK, Reaktoro_::Pressure
     auto G_bb = H298 - T*S298 + Pbar*V298 + b*( T298*T - pow(T298,2)/2 - pow(T,2)/2 ) + ( Cp298 - T298*b )/( T298*dALPdTw298 )*( ALPw298*(T - T298) - BETw298*Pbar + (T/Tprime)*log(RHOw/RHOw298) );
     auto G = G298 - (T - T298)*S298 + Pbar*V298 + b*( T298*T - pow(T298,2)/2 - pow(T,2)/2 ) + ( Cp298 - T298*b )/( T298*dALPdTw298 )*( ALPw298*(T - T298) - BETw298*Pbar + (T/Tprime)*log(RHOw/RHOw298) );
     auto S = S298 - b*(T298 - T) - (Cp298 - T298*b)/(T298*dALPdTw298)*(ALPw298 - log(RHOw/RHOw298)/Tprime - (T/Tprime)*ALPw);
-    auto So = S298 - b*(T298 - T) - ((Cp298 - T298*b)*(ALPw298 + log(RHOw/RHOw298)/Tprime))/(T298*dALPdTw298);
     auto V = V298 + (Cp298 - T298*b)/(T298*dALPdTw298)*( -BETw298 + T/Tprime*BETw );
-    auto Cp = b/T - (Cp298 - T298*b)/(T298*dALPdTw298)*( dALPdTw - 2*ALPw/Tprime - T/Tprime*dALPdTw );
+    auto Cp = T*(b - ((T298*b - Cp298)*(T*dALPdTw))/(T298*dALPdTw298*Tprime));
     auto H = G_bb + T*S298;
 
     tps.gibbs_energy     = G;
