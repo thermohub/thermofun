4e0806cab07d50b420d750dccea6bfa05a8089d1
dmiron <mirondanro@yahoo.com>
2019-09-14 13:56:26 +0200

    fix build windows

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -7,6 +7,6 @@ if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
 )
 
 echo "Configuring..."
-cmake -S . -B build
+cmake -S . -B -G"Visual Studio 15 2017" -A x64 build
 echo "Building..."
 cmake --build build --config %CONFIGURATION% --target install
