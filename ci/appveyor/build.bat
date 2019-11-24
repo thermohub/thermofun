b1bac5b13b88b215cb848273ea7cbeae2ba593fb
dmiron <mirondanro@yahoo.com>
2019-11-24 22:28:45 +0100

    fixed path to python

--- ci/appveyor/build.bat
+++ ci/appveyor/build.bat
@@ -1,12 +1,3 @@
-if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-)
-if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
-    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-)
-
-echo "Configuring..."
 for /f "usebackq skip=1" %%i in (`where python`) do (
   set PREFIX=%%i
   goto :done
@@ -22,6 +13,15 @@ goto:eof
 set path=%~dp1
 echo %path%
 
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+if "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
+    call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+)
+
+echo "Configuring..."
 cmake -G"Visual Studio 15 2017" -DTHERMOFUN_PYTHON_INSTALL_PREFIX:PATH=%path% -A x64 -S . -B build
 echo "Building..."
 cmake --build build --config %CONFIGURATION% --target install
