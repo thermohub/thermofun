e3c2458883e662ca02e4c853374f4fc8fe0fff3f
Geroge Dan Miron <44091914+gdmiron@users.noreply.github.com>
2020-02-17 13:50:22 +0100

    Origin bucket (#18)
    * Database object can now be constructed using a file or a string containing a ThermoDataSet in valid JSON
    
    * fixed division by zero problem in the propagation of derivatives
    
    * fix calculation of derivatives in Cp f(T) method at reference T-P
    
    * example properties reaction
    
    * bug-fix entropy of reaction

--- ThermoFun/Substances/EmpiricalCpIntegration.cpp
+++ ThermoFun/Substances/EmpiricalCpIntegration.cpp
@@ -5,25 +5,26 @@
 #include "Substance.h"
 #include <iomanip>
 
-namespace ThermoFun {
+namespace ThermoFun
+{
 
 auto thermoPropertiesEmpCpIntegration(Reaktoro_::Temperature TK, Reaktoro_::Pressure /*Pbar*/, Substance substance) -> ThermoPropertiesSubstance
 {
-    ThermoPropertiesSubstance   thermo_properties_PT   = substance.thermoProperties();
-    ThermoPropertiesSubstance   thermo_properties_PrTr = substance.thermoReferenceProperties();
-    SubstanceClass::type        substance_class        = substance.substanceClass();
-    ThermoParametersSubstance   thermo_parameters      = substance.thermoParameters();
+    ThermoPropertiesSubstance thermo_properties_PT = substance.thermoProperties();
+    ThermoPropertiesSubstance thermo_properties_PrTr = substance.thermoReferenceProperties();
+    SubstanceClass::type substance_class = substance.substanceClass();
+    ThermoParametersSubstance thermo_parameters = substance.thermoParameters();
 
     Reaktoro_::ThermoScalar V;
-    int k=-1;
+    int k = -1;
     vector<double> ac;
-    auto TK_=TK;
-    for (unsigned i = 0; i <16; i++)
+    auto TK_ = TK;
+    for (unsigned i = 0; i < 16; i++)
     {
         ac.push_back(0.0);
     }
 
-    auto TrK = substance.referenceT()/* + C_to_K*/;
+    auto TrK = substance.referenceT() /* + C_to_K*/;
 
     auto S = thermo_properties_PrTr.entropy;
     auto G = thermo_properties_PrTr.gibbs_energy;
@@ -35,16 +36,8 @@ auto thermoPropertiesEmpCpIntegration(Reaktoro_::Temperature TK, Reaktoro_::Pres
         return thermo_properties_PrTr;
     }
 
-//    aW.twp->devG = dc[q].Gs[1];
-//    ??????????????????????????????????????'
-//    if( !dc[q].Cp )
-//    {
-//        aW.twp->Cp = (double)dc[q].Cps[0];
-//        goto NEXT;
-//    }
-
     // get Cp interval -> this has to go!!!!
-    for (size_t i=0; i<thermo_parameters.temperature_intervals.size(); i++)
+    for (size_t i = 0; i < thermo_parameters.temperature_intervals.size(); i++)
     {
         if (thermo_parameters.temperature_intervals[i].size() > 0)
         {
@@ -60,120 +53,134 @@ auto thermoPropertiesEmpCpIntegration(Reaktoro_::Temperature TK, Reaktoro_::Pres
         }
     }
 
-    if (k<0)
+    if (k < 0)
     {
         if (TK_ < thermo_parameters.temperature_intervals[0][0])
             k = 0;
-        if (TK_ > thermo_parameters.temperature_intervals[thermo_parameters.temperature_intervals.size()-1][1])
-            k = thermo_parameters.temperature_intervals.size()-1;
-        std::cout << "The given temperature: "<< TK_ <<" is not inside the specified interval/s for the Cp calculation.";
-        std::cout << "The temperature is not inside the specified interval for the substance "<< substance.symbol() << "." << std::endl << __FILE__ << std::endl << __LINE__;
-//        Exception exception;
-//        exception.error << "The given temperature: "<< TK_ <<" is not inside the specified interval/s for the Cp calculation.";
-//        exception.reason << "The temperature is not inside the specified interval for the substance "<< substance.symbol() << ".";
-//        exception.file = __FILE__;
-//        exception.line = __LINE__;
-//        RaiseError(exception)
+        if (TK_ > thermo_parameters.temperature_intervals[thermo_parameters.temperature_intervals.size() - 1][1])
+            k = thermo_parameters.temperature_intervals.size() - 1;
+        std::cout << "The given temperature: " << TK_ << " is not inside the specified interval/s for the Cp calculation.";
+        std::cout << "The temperature is not inside the specified interval for the substance " << substance.symbol() << "." << std::endl
+                  << __FILE__ << std::endl
+                  << __LINE__;
     }
 
+    k = 0;
 
-
-    k=0;
-
-    for (unsigned i=0; i<thermo_parameters.Cp_coeff[k].size(); i++)
+    for (unsigned i = 0; i < thermo_parameters.Cp_coeff[k].size(); i++)
     {
-        if (i== 16) break;
+        if (i == 16)
+            break;
         ac[i] = thermo_parameters.Cp_coeff[k][i];
     }
 
-//    ac = thermo_parameters.Cp_coeff[k];
+    auto Cp = (ac[0] +
+                ac[1] * TK +
+                ac[2] / (TK * TK) +
+                ac[3] / (pow(TK, 0.5)) +
+                ac[4] * (TK * TK) +
+                ac[5] * (TK * TK * TK) +
+                ac[6] * (TK * TK * TK * TK) +
+                ac[7] / (TK * TK * TK) +
+                ac[8] / TK +
+                ac[9] * (pow(TK, (1 / 2))) /*+ ac[10]*log(T)*/);
 
-    auto Cp = ( ac[0] + ac[1]*TK + ac[2]/(TK*TK) + ac[3]/(pow(TK,0.5)) + ac[4]*(TK*TK)
-          + ac[5]*(TK*TK*TK) + ac[6]*(TK*TK*TK*TK) + ac[7]/(TK*TK*TK) + ac[8]/TK + ac[9]*(pow(TK,(1/2))) /*+ ac[10]*log(T)*/);
-
-    // Phase transitions
-    if (fabs(TK.val-TrK) > TEMPER_PREC)
+    for (unsigned j = 0, ft = 0; j <= k; j++)
     {
-        for (unsigned j=0, ft = 0; j<=k; j++)
+        if (j == k)
+            TK = TK_.val /* + C_to_K*/; // current T is the end T for phase transition Cp calculations
+        else
+            TK = thermo_parameters.temperature_intervals[j][1] /*+ C_to_K*/; // takes the upper bound from the j-th Tinterval
+
+        if (!j)
+            TrK = substance.referenceT() /*+ C_to_K*/; // if j=0 the first interval should contain the reference T (Tcr)
+        else
+            TrK = thermo_parameters.temperature_intervals[j][0] /*+ C_to_K*/; // if j>0 then we are in a different Tinterval and the reference T becomes the lower bound of the interval
+
+        auto Tst2 = TrK * TrK;
+        auto Tst3 = Tst2 * TrK;
+        auto Tst4 = Tst3 * TrK;
+        auto Tst05 = std::sqrt(TrK);
+
+        // going trough the phase transitions parameters in FtP
+        //            for (unsigned ft = 0; ft < thermo_parameters.phase_transition_prop.size(); ft++)
+
+        if (j && thermo_parameters.phase_transition_prop.size() == 0)
         {
-            if ( j == k )
-                TK = TK_.val/* + C_to_K*/;     // current T is the end T for phase transition Cp calculations
-            else TK = thermo_parameters.temperature_intervals[j][1] /*+ C_to_K*/;        // takes the upper bound from the j-th Tinterval
-
-            if( !j )
-                TrK = substance.referenceT() /*+ C_to_K*/;            // if j=0 the first interval should contain the reference T (Tcr)
-            else  TrK = thermo_parameters.temperature_intervals[j][0] /*+ C_to_K*/;    // if j>0 then we are in a different Tinterval and the reference T becomes the lower bound of the interval
-
-            auto Tst2 = TrK * TrK;
-            auto Tst3 = Tst2 * TrK;
-            auto Tst4 = Tst3 * TrK;
-            auto Tst05 = std::sqrt( TrK );
-
-            // going trough the phase transitions parameters in FtP
-//            for (unsigned ft = 0; ft < thermo_parameters.phase_transition_prop.size(); ft++)
-
-            if (j && thermo_parameters.phase_transition_prop.size() == 0)
-            {
-                Exception exception;
-                exception.error << "No phase transition properties present in the record.";
-                exception.reason << "For substance "<< substance.symbol() << ".";
-                exception.line = __LINE__;
-                RaiseError(exception);
-            }
-
-            if ( j && thermo_parameters.phase_transition_prop[ft][0] <= TrK/*-C_to_K*/ )
-            {   // Adding parameters of phase transition
-                if ( thermo_parameters.phase_transition_prop[ft].size() > 1 )  // dS
-                    S += thermo_parameters.phase_transition_prop[ft][1];
-                if ( thermo_parameters.phase_transition_prop[ft].size() > 2 )  // dH
-                    H += thermo_parameters.phase_transition_prop[ft][2];
-                if ( thermo_parameters.phase_transition_prop[ft].size() > 3 )  // dV
-                    V += thermo_parameters.phase_transition_prop[ft][3];
-                // More to be added ?
-                ft++;
-            }
-
-            G -= S * (TK - TrK);
-
-            for (unsigned i=0; i<thermo_parameters.Cp_coeff[j].size(); i++)
-            {
-                if (i== 16) break;
-                ac[i] = thermo_parameters.Cp_coeff[j][i];
-            }
-
-
-            S += ( ac[0] * log( (TK/TrK) ) + ac[1] * (TK - TrK) + ac[2] * ( 1./Tst2 - 1./(TK*TK) ) / 2.
-                 + ac[3] * 2. * ( 1./Tst05 - 1./(pow(TK,0.5)) ) + ac[4] * ( (TK*TK) - Tst2 ) / 2.
-                 + ac[5] * ( (pow(TK,3)) - Tst3 ) / 3. + ac[6] * ( (pow(TK,4)) - Tst4 ) / 4.
-                 + ac[7] * ( 1./ Tst3 - 1./ (pow(TK,3)) ) / 3. + ac[8] * (1. / TrK - 1. / TK )
-                 + ac[9] * 2.* ( (pow(TK,0.5)) - Tst05 ) );
-
-            G -= ( ac[0] * ( TK * log( (TK/TrK) ) - (TK - TrK) ) + ac[1] * (pow((TK - TrK),2)) / 2. + ac[2] * (pow((TK - TrK),2)) / TK / Tst2 / 2.
-                 + ac[3] * 2. * ((pow(TK,0.5)) - Tst05)*((pow(TK,0.5)) - Tst05) / Tst05 + ac[4] * ( (pow(TK,3)) + 2.*Tst3 - 3.* TK * Tst2 ) / 6.
-                 + ac[5] * ( (pow(TK,4)) + 3.*Tst4 - 4.*TK * Tst3 ) / 12. + ac[6] * ( (pow(TK,4))*TK + 4.*Tst4*TrK - 5.*TK*Tst4 ) / 20.
-                 + ac[7] * ( Tst3 - 3.* (pow(TK,2)) * TrK + 2.*(pow(TK,3)) ) / 6./ (pow(TK,2)) / Tst3 + ac[8] * ( (TK/TrK) - 1. - log( (TK/TrK) ))
-                 + ac[9] * 2.* ( 2.* TK * (pow(TK,0.5)) - 3.* TK * Tst05 + TrK * Tst05 ) / 3. );
-
-            H += ( ac[0] * (TK - TrK) + ac[1] * ( (pow(TK,2)) - Tst2 ) / 2. + ac[2] * ( 1./TrK - 1./TK )
-                 + ac[3] * 2. * ( (pow(TK,0.5)) - Tst05 ) + ac[4] * ( (pow(TK,3)) - Tst3 ) / 3.
-                 + ac[5] * ( (pow(TK,4)) - Tst4 ) / 4. + ac[6] * ( (pow(TK,4)) * TK - Tst4 * TrK ) / 5
-                 + ac[7] * ( 1./ Tst2 - 1./ (pow(TK,2)) ) / 2. + ac[8] * log( (TK/TrK) )
-                 + ac[9] * 2.* ( TK * (pow(TK,0.5)) - TrK * Tst05 ) / 3. );
+            Exception exception;
+            exception.error << "No phase transition properties present in the record.";
+            exception.reason << "For substance " << substance.symbol() << ".";
+            exception.line = __LINE__;
+            RaiseError(exception)
         }
+
+        if (j && thermo_parameters.phase_transition_prop[ft][0] <= TrK /*-C_to_K*/)
+        {                                                               // Adding parameters of phase transition
+            if (thermo_parameters.phase_transition_prop[ft].size() > 1) // dS
+                S += thermo_parameters.phase_transition_prop[ft][1];
+            if (thermo_parameters.phase_transition_prop[ft].size() > 2) // dH
+                H += thermo_parameters.phase_transition_prop[ft][2];
+            if (thermo_parameters.phase_transition_prop[ft].size() > 3) // dV
+                V += thermo_parameters.phase_transition_prop[ft][3];
+            // More to be added ?
+            ft++;
+        }
+
+        G -= S * (TK - TrK);
+
+        for (unsigned i = 0; i < thermo_parameters.Cp_coeff[j].size(); i++)
+        {
+            if (i == 16)
+                break;
+            ac[i] = thermo_parameters.Cp_coeff[j][i];
+        }
+
+        S += (ac[0] * log((TK / TrK)) +
+                ac[1] * (TK - TrK) +
+                ac[2] * (1. / Tst2 - 1. / (TK * TK)) / 2. +
+                ac[3] * 2. * (1. / Tst05 - 1. / (pow(TK, 0.5))) +
+                ac[4] * ((TK * TK) - Tst2) / 2. +
+                ac[5] * ((pow(TK, 3)) - Tst3) / 3. +
+                ac[6] * ((pow(TK, 4)) - Tst4) / 4. +
+                ac[7] * (1. / Tst3 - 1. / (pow(TK, 3))) / 3. +
+                ac[8] * (1. / TrK - 1. / TK) +
+                ac[9] * 2. * ((pow(TK, 0.5)) - Tst05));
+
+        G -= (ac[0] * (TK * log((TK / TrK)) - (TK - TrK)) +
+                ac[1] * (pow((TK - TrK), 2)) / 2. +
+                ac[2] * (pow((TK - TrK), 2)) / TK / Tst2 / 2. +
+                ac[3] * 2. * ((pow(TK, 0.5)) - Tst05) * ((pow(TK, 0.5)) - Tst05) / Tst05 +
+                ac[4] * ((pow(TK, 3)) + 2. * Tst3 - 3. * TK * Tst2) / 6. +
+                ac[5] * ((pow(TK, 4)) + 3. * Tst4 - 4. * TK * Tst3) / 12. +
+                ac[6] * ((pow(TK, 4)) * TK + 4. * Tst4 * TrK - 5. * TK * Tst4) / 20. +
+                ac[7] * (Tst3 - 3. * (pow(TK, 2)) * TrK + 2. * (pow(TK, 3))) / 6. / (pow(TK, 2)) / Tst3 +
+                ac[8] * ((TK / TrK) - 1. - log((TK / TrK))) +
+                ac[9] * 2. * (2. * TK * (pow(TK, 0.5)) - 3. * TK * Tst05 + TrK * Tst05) / 3.);
+
+        H += (ac[0] * (TK - TrK) +
+                ac[1] * ((pow(TK, 2)) - Tst2) / 2. +
+                ac[2] * (1. / TrK - 1. / TK) +
+                ac[3] * 2. * ((pow(TK, 0.5)) - Tst05) +
+                ac[4] * ((pow(TK, 3)) - Tst3) / 3. +
+                ac[5] * ((pow(TK, 4)) - Tst4) / 4. +
+                ac[6] * ((pow(TK, 4)) * TK - Tst4 * TrK) / 5 +
+                ac[7] * (1. / Tst2 - 1. / (pow(TK, 2))) / 2. +
+                ac[8] * log((TK / TrK)) +
+                ac[9] * 2. * (TK * (pow(TK, 0.5)) - TrK * Tst05) / 3.);
     }
 
-    thermo_properties_PT.heat_capacity_cp   = Cp;
-    thermo_properties_PT.gibbs_energy       = G;
-    thermo_properties_PT.enthalpy           = H;
-    thermo_properties_PT.entropy            = S;
-    thermo_properties_PT.volume             = V;
+    thermo_properties_PT.heat_capacity_cp = Cp;
+    thermo_properties_PT.gibbs_energy = G;
+    thermo_properties_PT.enthalpy = H;
+    thermo_properties_PT.entropy = S;
+    thermo_properties_PT.volume = V;
 
-    if (k<0)
+    if (k < 0)
     {
-        setMessage(Reaktoro_::Status::calculated,"Empirical Cp integration: Outside temperature bounds", thermo_properties_PT );
+        setMessage(Reaktoro_::Status::calculated, "Empirical Cp integration: Outside temperature bounds", thermo_properties_PT);
     }
 
     return thermo_properties_PT;
 }
 
-}
+} // namespace ThermoFun
