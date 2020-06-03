5c137e5f9133c06a9063abc7d70044125c7df6e2
George Dan Miron <mirondanro@yahoo.com>
2020-06-03 13:06:18 +0000

    install.bat edited online with Bitbucket

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,7 +1,6 @@
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels conda-forge
-conda clean --packages
-conda install conda-devenv
+conda config --system --set always_yes yes --set changeps1 no
+conda config --system --append channels conda-forge
+conda install -n base conda-devenv
 conda update -q conda
 conda info -a
 conda devenv
