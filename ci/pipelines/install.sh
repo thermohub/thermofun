66baf48c9dc0ef29a68107975a9a0aedb0fce577
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2020-12-15 18:09:12 +0100

    Update install.sh

--- ci/pipelines/install.sh
+++ ci/pipelines/install.sh
@@ -27,6 +27,11 @@ cmake -GNinja \
     -DCMAKE_INSTALL_LIBDIR=lib \
     ..
 ninja install
+if [ $? -eq 1 ]
+then
+echo "The install failed" >&2
+exit 1
+fi
 conda list
 cd ..
 pytest -ra -vv --color=yes .
