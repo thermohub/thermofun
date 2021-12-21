2eafb44f6daa6beb11c5474d9e475516b4bde39b
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2021-12-21 13:07:35 +0100

    Update install.bat

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,5 +1,5 @@
-conda install conda=4.9.2
 conda config --set always_yes yes --set changeps1 no
+conda install conda=4.9.2
 conda config --add channels conda-forge
 conda info -a
 conda clean --packages
