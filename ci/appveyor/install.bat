b2dbd43a2dd8af645520fe37c51956fd877f840d
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2021-12-21 13:20:10 +0100

    Update install.bat

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,8 +1,7 @@
 conda config --set always_yes yes --set changeps1 no
-conda install conda=4.9.2
+conda update conda --yes
 conda config --add channels conda-forge
 conda info -a
-conda clean --packages
 conda install conda-devenv
 conda devenv
 call activate thermofun
