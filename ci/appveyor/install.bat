bd43d3aa769496dfa810fe8c477a0175c133d675
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2021-12-21 12:20:01 +0100

    trying to fix appveyor

--- ci/appveyor/install.bat
+++ ci/appveyor/install.bat
@@ -1,7 +1,5 @@
-conda config --system --set always_yes yes --set changeps1 no
-conda config --system --append channels conda-forge
-conda install -n base conda-devenv
-conda update -q conda
-conda info -a
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels conda-forge
+conda install conda-devenv
 conda devenv
 call activate thermofun
