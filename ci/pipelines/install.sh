8599a68ee7f6108c3869d98d3422b784f027f24f
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2021-12-22 12:35:16 +0100

    Update install.sh

--- ci/pipelines/install.sh
+++ ci/pipelines/install.sh
@@ -22,9 +22,10 @@ cd build
 python_path=$(which python)
 # Configure step
 cmake -GNinja \
-    -DPYTHON_EXECUTABLE:FILEPATH=$python_path \
+    -DPYTHON_EXECUTABLE:FILEPATH="$PYTHON" \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_LIBDIR=lib \
+    -DCMAKE_INSTALL_LIBDIR=lib \    
     ..
 ninja install
 if [ $? -eq 1 ]
