ba73b20182965ca30bb6c5c9db8b8ddf3201de30
George Dan Miron <mirondanro@yahoo.com>
2022-06-28 09:04:50 +0000

    installVS.bat edited online with Bitbucket

--- ci/pipelines/installVS.bat
+++ ci/pipelines/installVS.bat
@@ -17,7 +17,7 @@ rem along with thermofun.  If not, see <http://www.gnu.org/licenses/>.
 
 call activate thermofun
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=19.29.30139.0"
 
 echo "Configuring..."
 cmake -G"Visual Studio 16 2019" -DTHERMOFUN_PYTHON_INSTALL_PREFIX:PATH="%CONDA_PREFIX%" -A x64 -S . -B build
