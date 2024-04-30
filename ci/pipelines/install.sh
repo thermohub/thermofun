a91c6d57e5ac876b284423845486b2372f4193bd
George Dan Miron <dan.miron@psi.ch>
2024-04-30 11:53:04 +0000

    install.sh edited online with Bitbucket

--- ci/pipelines/install.sh
+++ ci/pipelines/install.sh
@@ -1,6 +1,7 @@
 if [ ! -f $HOME/miniconda/bin/conda ]; then
     echo "Downloading and installing miniconda"
-    wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    alias wgetncc="wget --no-check-certificate"
+    wgetncc -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
     rm -rf $HOME/miniconda
     bash miniconda.sh -b -p $HOME/miniconda
 fi
