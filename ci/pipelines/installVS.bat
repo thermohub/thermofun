704f73cabe5e3948aaff6d2c47a71a09944ba672
George Dan Miron <44091914+gdmiron@users.noreply.github.com>
2025-05-05 09:34:58 +0200

    avoid parser errors, fixed windows path build error (#73)
    * avoid parser errors
    
    * removed paper
    
    * fix issue with path in windows
    
    ---------
    
    Co-authored-by: Svitlana <60260716+sdmytrievs@users.noreply.github.com>

--- ci/pipelines/installVS.bat
+++ ci/pipelines/installVS.bat
@@ -20,6 +20,8 @@ call activate thermofun
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat -vcvars_ver=19.29.30139.0"
 
 echo "Configuring..."
-cmake -G"Visual Studio 16 2019" -DTHERMOFUN_PYTHON_INSTALL_PREFIX:PATH="%CONDA_PREFIX%" -A x64 -S . -B build
+cmake -G"Visual Studio 16 2019" ^
+  -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%\Library" ^
+  -A x64 -S . -B build
 echo "Building..."
 cmake --build build --target install
